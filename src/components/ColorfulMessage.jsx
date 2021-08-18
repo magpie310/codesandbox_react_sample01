cd5572f42e6fb466e1e82a7fd7a0f187f100a090
magpie310 <51537530+magpie310@users.noreply.github.com>
2021-08-18 11:05:07 +0900

    完成

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const ColorfulMessage = (props) => {
+  const contentStyle = {
+    color: props.color,
+    fontSize: "18px"
+  };
+  return <p style={contentStyle}>{props.children}</p>;
+};
+export default ColorfulMessage;
